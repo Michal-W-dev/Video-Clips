7b0a002cb5b5387e7da9bda5fd0858363329f885
Michal-W-dev <maikey20@gmail.com>
2022-03-28 21:16:59 +0200

    Add shared.module & modal component.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -10,7 +10,7 @@ import { SharedModule } from '../shared/shared.module';
     AuthModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ],
   exports: [AuthModalComponent]
 })
