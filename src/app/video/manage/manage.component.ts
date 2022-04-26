0471df2f2aa7b6e5116bf5e729e58b2d6edf06a8
Michal-W-dev <maikey20@gmail.com>
2022-04-26 22:02:23 +0200

    Clip.service - get user clips from firebase & display clips in manage.component.

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, Params } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -8,11 +10,17 @@ import { ActivatedRoute, Router, Params } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1'
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  clips: IClip[] = []
+  constructor(private router: Router, private route: ActivatedRoute, private clipService: ClipService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1'
+    })
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = []
+      docs.forEach(doc => this.clips.push({ docID: doc.id, ...doc.data() }))
+      console.log(this.clips)
     })
   }
 
