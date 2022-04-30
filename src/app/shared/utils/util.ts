bcbb1a55b4f830f7c3f032a78d9310805b91850a
Michal-W-dev <maikey20@gmail.com>
2022-04-30 22:37:51 +0200

    Add Utils class with (resizeBase64 & snashooter) static methods. First method lower quality and size of the snapshot. Second method use clip to create array of snapshots. Npm video-snapshot

---
+++ src/app/shared/utils/util.ts
@@ -0,0 +1,41 @@
+import VideoSnapshot from "video-snapshot";
+
+export default class Utils {
+  static resizeBase64Img(base64: string, quality = .95, size = 1) {
+    return new Promise<string>(resolve => {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      const img = document.createElement("img");
+      img.src = base64;
+
+      img.onload = () => {
+        canvas.width = img.width * size;
+        canvas.height = img.height * size;
+        context?.drawImage(img, 0, 0, canvas.width, canvas.height);
+        resolve(canvas.toDataURL("image/jpeg", quality));
+      }
+    });
+  }
+  static async snapshooter(file: File) {
+    return new Promise<{ snapshootsArray: string[], isLoading: boolean }>((resolve, reject) => {
+      const snapshoter = new VideoSnapshot(file);
+      const vid = document.createElement('video');
+      vid.src = snapshoter.videoUrl
+
+      // If file is not supported, no dimension can be found (which result with reject Promise).
+      snapshoter.getDimensions().catch(e => reject(e))
+      vid.onloadedmetadata = () => {
+        const durations = [0.1, vid.duration / 4, vid.duration / 2.5, vid.duration / 1.8, vid.duration / 1.4, vid.duration - 1]
+        const snapshootsArray: string[] = []
+        durations.forEach((dur, i) => {
+          snapshoter.takeSnapshot(dur).then(imgSrc => {
+            Utils.resizeBase64Img(imgSrc, .8, .55).then(imgLowQuality => {
+              snapshootsArray.push(imgLowQuality);
+              if (durations.length - 1 === i) resolve({ snapshootsArray, isLoading: false })
+            });
+          })
+        })
+      }
+    })
+  }
+}
